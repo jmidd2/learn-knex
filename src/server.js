37f0d08b410397bb3bf6167ba40246390f208f77
FlippantPope <jonpmiddleton@gmail.com>
2023-07-03 10:55:15 -0700

    Fixed eslint issues

--- src/server.js
+++ src/server.js
@@ -3,12 +3,27 @@
 /**
  * Module dependencies.
  */
-import app from './app.js';
 import http from 'http';
+import app from './app.js';
 
 /**
  * Get port from environment and store in Express.
  */
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (Number.isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
 
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
@@ -30,22 +45,6 @@ server.listen(port, () => {
 
   console.log(`Server listening at http://${host}:${port}`);
 });
-
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
 
 process.on('SIGTERM', () => {
   console.debug('SIGTERM signal received: closing HTTP server...');
